54e3f3e29c82803879fd3b6687ed134337973678
Yurim Jin <jayjinjay@gmail.com>
2021-12-29 18:58:38 +0900

    챕터 0, 1 코드 추가가

---
+++ my-utils.js
@@ -0,0 +1,17 @@
+const EMPTY_HEART = "🤍";
+const FULL_HEART = "💖";
+
+const CAT1 = "https://cataas.com/cat/60b73094e04e18001194a309/says/react";
+const CAT2 = "https://cataas.com//cat/5e9970351b7a400011744233/says/inflearn";
+const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
+
+const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
+
+const myLocalStorage = {
+  setItem: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getItem: (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  },
+};
