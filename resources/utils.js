7e0088619937b1af998940f431b6ff2a2ee977e1
Yurim Jin <jayjinjay@gmail.com>
2022-01-01 22:25:30 +0900

    21 코드 정리하기

--- resources/utils.js
+++ resources/utils.js
@@ -7,7 +7,7 @@ const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
 
 const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
 
-const myLocalStorage = {
+const jsonLocalStorage = {
   setItem: (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   },
@@ -16,9 +16,8 @@ const myLocalStorage = {
   },
 };
 
-const OPEN_API_DOMAIN = "https://cataas.com";
-
 const fetchCat = async (text) => {
+  const OPEN_API_DOMAIN = "https://cataas.com";
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
   const responseJson = await response.json();
   return `${OPEN_API_DOMAIN}/${responseJson.url}`;
