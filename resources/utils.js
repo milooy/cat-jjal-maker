fead5333f2fe26e56e6f2701b774fec2a0799c89
Yurim Jin <jayjinjay@gmail.com>
2022-01-01 20:09:52 +0900

    19 폼 다루기

--- resources/utils.js
+++ resources/utils.js
@@ -16,9 +16,10 @@ const myLocalStorage = {
   },
 };
 
-const OPEN_API_DOMAIN = 'https://cataas.com'
+const OPEN_API_DOMAIN = "https://cataas.com";
+
 const fetchCat = async (text) => {
-  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`)
-  const responseJson = await response.json()
-  return `${OPEN_API_DOMAIN}/${responseJson.url}`
-}
+  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
+  const responseJson = await response.json();
+  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+};
