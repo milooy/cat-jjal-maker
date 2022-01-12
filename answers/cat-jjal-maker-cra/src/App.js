c6dad0b8ebf850e47121dfe528c790af37982b89
Yurim Jin <jayjinjay@gmail.com>
2022-01-12 18:02:06 +0900

    CRA도 answers로 옮기기

--- answers/cat-jjal-maker-cra/src/App.js
+++ answers/cat-jjal-maker-cra/src/App.js
@@ -53,7 +53,7 @@ const Form = ({ updateMainCat }) => {
         value={value}
         onChange={handleInputChange}
       />
-      <button type="submit">생성2</button>
+      <button type="submit">생성</button>
       <p style={{ color: "red" }}>{errorMessage}</p>
     </form>
   );
