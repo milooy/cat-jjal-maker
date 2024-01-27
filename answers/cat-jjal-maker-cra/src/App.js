3dce7e6e854b3835cc3001b3e323b8ee4281f2de
yurimjin <jayjinjay@gmail.com>
2024-01-27 17:29:55 +0900

    fix: api 필드 변경

--- answers/cat-jjal-maker-cra/src/App.js
+++ answers/cat-jjal-maker-cra/src/App.js
@@ -16,7 +16,7 @@ const fetchCat = async (text) => {
   const OPEN_API_DOMAIN = "https://cataas.com";
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
   const responseJson = await response.json();
-  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+  return `${OPEN_API_DOMAIN}/cat/${responseJson._id}/says/${text}`;
 };
 
 const Form = ({ updateMainCat }) => {
--- resources/utils.js
+++ resources/utils.js
@@ -20,5 +20,5 @@ const fetchCat = async (text) => {
   const OPEN_API_DOMAIN = "https://cataas.com";
   const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
   const responseJson = await response.json();
-  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+  return `${OPEN_API_DOMAIN}/cat/${responseJson._id}/says/${text}`; // NOTE: API 스펙 변경으로 강의 영상과 다른 URL로 변경했습니다.
 };
